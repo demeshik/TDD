c7568442e247bab012a090cc86d79220d1fb4c35
Alexander Demeshchik <djrazor308@hotmail.com>
2017-06-12 10:37:44 +0300

    init

---
+++ models/Team.js
@@ -0,0 +1,11 @@
+module.exports = (Sequelize, sequelize) => {
+    return sequelize.define('teams', {
+        id: {
+            type: Sequelize.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+
+        name        : Sequelize.STRING
+    });
+};
